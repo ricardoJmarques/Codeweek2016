2f052cac63489c1d38c8a94c44263cedffd9fc98
ricardoJmarques <a55723@alunos.uminho.pt>
2016-10-23 00:17:42 +0100

    Update desafio1-identado.c

--- desafio1-identado.c
+++ desafio1-identado.c
@@ -1,28 +1,28 @@
 #include<stdio.h>
 int main(){
-	char A[4096],B[99],C[26]={0},D[26],E[4096];//D chave expandida
+	char A[4096],B[99],C[26]={0},D[26],E[4096];
 	int F=0,i,j,k;i=0;
-	scanf("%s %d\n%s",B,&F,E); //B chave; F numero; E dados a encriptar
+	scanf("%s %d\n%s",B,&F,E);
 	i=j=0;
-	while(B[i]!=0){//cria chave expandida
-		if(C[(B[i]-'a')]==0){//C[] é uma tabela auxiliar que marca as letras já ocorridas
+	while(B[i]!=0){
+		if(C[(B[i]-'a')]==0){
 			C[B[i]-'a']=1;
-			D[j]=B[i];//preenche a chave expandida 
+			D[j]=B[i]; 
 			j++;}
 		i++;}
-	for(i=0;i<26;i++){//preenche a chave expandida com as letras em falta
+	for(i=0;i<26;i++){
 		if(C[i]==0){
-			D[j]=i+'a';//cria a primeira tabela de substituição 
+			D[j]=i+'a'; 
 			j++;}}
 	i=0;
-	while(E[i]!=0){//faz primeira substituicao
+	while(E[i]!=0){
 		j=0;
-			while(D[j]!=E[i])//calcula deslocamento
+			while(D[j]!=E[i])
 				j++;
-		E[i]=j%10+1;//substitui o caracter
+		E[i]=j%10+1;
 		i++;}
 	i=j=k=0;
-	while(E[i]!=0){//faz a segunda substituição
+	while(E[i]!=0){
 		if(j<F)
 			j=j*10+E[i]-1;
 		else{
@@ -30,7 +30,7 @@ int main(){
 			j=E[i]-1;
 			k++;}
 		i++;}
-	A[k++]=D[j-F];//adiciona o ultimo byte
-	A[k++]=0;//adiciona o terminador á string
+	A[k++]=D[j-F];
+	A[k++]=0;
 	puts(A);
 	return 0;}
